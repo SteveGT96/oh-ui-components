ef8423aaa240a4b7dcb0a28dc0a7bc221f03f747
Alessia Menon <alessia.menon@intesys.it>
2025-12-17 17:41:01 +0100

    fix(SD-24): standardize export syntax in index files

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,5 +2,4 @@
 // Example: export { default as Button } from './Button'
 // Example: export { default as Card } from './Card'
 
-// For now, we'll export a placeholder
-export const version = '1.0.0-alpha.1'
+export const version = "1.0.0-alpha.1";
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 // Export all components and utilities from the library
-export * from './components'
-export * from './utils'
+export * from "./components";
+export * from "./utils";
